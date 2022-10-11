d50cd10883c2901ba80cfa537d00d2df11b45d4b
Neil Stoker <neil.stoker@clixifix.co.uk>
2022-10-11 11:28:30 +0100

    add debug line

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   FOLLOWER_URL = ENV.fetch('FOLLOWERUSER_URL', nil)
+  Rails.logger.error "¬¬ #{Rails.application.config.database_configuration[Rails.env]}"
 end
