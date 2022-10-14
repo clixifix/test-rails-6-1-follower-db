b06f56d15c87e6552d21988fde5c9124d52edf0d
Neil Stoker <neil.stoker@clixifix.co.uk>
2022-10-14 11:06:15 +0100

    add delayed_jobs gem and initialise

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module TestRails61FollowerDb
     #
     # config.time_zone = 'Central Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join('extras')
+
+    config.active_job.queue_adapter = :delayed_job
   end
 end
